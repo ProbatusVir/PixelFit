2067137558420682068e34e778c1e56fc2d88851
Joshua <joshbear367763@gmail.com>
2024-11-13 20:16:47 -0500

    Added methods to accommodate server-oriented data flow

--- app/src/main/java/User.kt
+++ app/src/main/java/User.kt
@@ -1,17 +1,18 @@
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.toDrawable
-import java.io.File
 import kotlin.random.Random
 
 //TODO: This is a far from complete class, will have to work on server integration.
 open class User {
-    lateinit var username : String;
-    lateinit var displayName : String;
-    private lateinit var profileImagePath : String;
+    lateinit var username : String
+    lateinit var displayName : String
+    private lateinit var profileImagePath : String
+
+
+    fun loadFromServer() {}
+    fun sendToServer() {}
 
     public fun requestUser() {
         //TODO: Get info from server and initialize our information
@@ -35,7 +36,7 @@ open class User {
 
     private fun randomPath() : String {
         val pathSize = 8
-        var str : String = String()
+        var str = String()
 
         //There is a randomSequence method for strings, but it's not guaranteed to be filesystem safe.
 
