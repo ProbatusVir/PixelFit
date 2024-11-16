77923a825ad21c6beb4067dd67fce9c15f5001e6
Joshua <joshbear367763@gmail.com>
2024-11-16 18:28:43 -0500

    Added some (currently dead and leaking memory) code to start developing the SQL interface alongside the server

--- ServerPackage/ServerPackage.cpp
+++ ServerPackage/ServerPackage.cpp
@@ -3,12 +3,14 @@
 
 
 #include "WindowsServer.h"
+#include "SQLInterface.h"
 
 int main()
 {
 #if defined(_WIN32)
 
 	WindowsServer server;
+	SQLInterface* sql_server = SQLInterface::Instance();
 	if (server.IPSetupComplete()) {
 		server.Start();
 	}
