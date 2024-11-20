6ea2ec966dae40586fc7518bfaf48ae6d85f43b2
Rashid Byrdsell <RHByrdsell@outlook.com>
2024-11-19 19:39:10 -0500

    Nav Bar and Profile visibility on log in screen

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSecondBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -40,5 +42,15 @@ class SecondFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.visibility = View.GONE
+
+        val profile = requireActivity().findViewById<ImageButton>(R.id.user_avatar)
+        profile.visibility = View.GONE
     }
 }
