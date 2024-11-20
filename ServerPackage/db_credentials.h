41ec78bfaf798d24d9f7f741165a933716794d5b
Joshua <joshbear367763@gmail.com>
2024-11-19 20:55:18 -0500

    Good luck

--- ServerPackage/db_credentials.h
+++ ServerPackage/db_credentials.h
@@ -4,6 +4,7 @@
 // https://www.dropbox.com/scl/fi/wlozzlt77ib21d1wdj22n/login_credentials.png?rlkey=wkqn7m9pv9rc1ob8fk8qw0lfx&st=w7pg8y69&dl=0
 // Make sure your server is properly started before you attempt to log in, or it simply won't work.
 
-constexpr unsigned char db_name[] = "";
-constexpr unsigned char auth_username[] = "";
-constexpr unsigned char auth_password[] = "";
+constexpr char db_name[] = "";
+constexpr char auth_username[] = "";
+constexpr char auth_password[] = "";
+constexpr char dsn[] = "";
