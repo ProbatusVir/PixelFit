d55c331631b74faa7dd4304a074970eebee3e294
Devin McLaughlin <DRMcLaughlin@student.fullsail.edu>
2024-11-19 19:05:33 -0500

    Added button split file and updated text and positioning

--- app/src/main/java/com/example/myapplication/FitnessNavFragment.kt
+++ app/src/main/java/com/example/myapplication/FitnessNavFragment.kt
@@ -32,7 +32,7 @@ class FitnessNavFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
+        binding.loginButton.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
