1da94d9016e0c74c6234164cd90428e90897d67a
Joshua <joshbear367763@gmail.com>
2024-11-20 12:09:06 -0500

    Commented, added some debug code, removed some code handled elsewhere, fixed invalid types

--- ServerPackage/db_credentials.h
+++ ServerPackage/db_credentials.h
@@ -4,7 +4,17 @@
 // https://www.dropbox.com/scl/fi/wlozzlt77ib21d1wdj22n/login_credentials.png?rlkey=wkqn7m9pv9rc1ob8fk8qw0lfx&st=w7pg8y69&dl=0
 // Make sure your server is properly started before you attempt to log in, or it simply won't work.
 
-constexpr char db_name[] = "";
-constexpr char auth_username[] = "";
-constexpr char auth_password[] = "";
-constexpr char dsn[] = "";
+#define db_name ""
+#define auth_username ""
+#define auth_password ""
+#define dsn ""
+
+// Don't touch.
+#define field1 "DSN="
+#define field2 ";UID="
+#define field3 ";Trusted_Connection=Yes"
+#define field4 ";WSID="
+
+//DSN=LOCAL_DSN;UID=USER;Trusted_Connection=Yes;WSID=SERVERHOST;
+
+constexpr const char* inConnStr = field1 dsn field2 auth_username field3 field4 db_name;
