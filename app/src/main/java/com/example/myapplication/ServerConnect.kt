6af8d82562d06d48b0eceda615ff021fee161a7a
Joshua <joshbear367763@gmail.com>
2024-11-21 15:07:54 -0500

    Threaded this

--- app/src/main/java/com/example/myapplication/ServerConnect.kt
+++ app/src/main/java/com/example/myapplication/ServerConnect.kt
@@ -1,34 +1,40 @@
 package com.example.myapplication
-import android.content.Context
-import android.net.wifi.WifiManager
 import android.os.StrictMode
-import android.text.format.Formatter.formatIpAddress
+import java.io.IOException
 import java.net.InetAddress
 import java.net.Socket
-import java.util.*
-
 
 class ServerConnect() {
 
 
-    val serverAddress = getMyServerAddress()
-    val serverPort = 5930
+    private val serverName = "10.0.2.2"
+    private val serverPort = 5930
+
+    private val serverAddress = getMyServerAddress()
     //TODO: add some error handling
-    val socket = getMySocket()
+    val socket : Socket? = getMySocket()
 
     private fun getMyServerAddress() : InetAddress {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy);
 
-        //NOTE: When testing on the local NETWORK, you must rebuild the project with
-        // the server's address hardcoded here.
-        return InetAddress.getByName("10.0.2.2")     //https://stackoverflow.com/questions/5806220/how-to-connect-to-my-http-localhost-web-server-from-android-emulator
+        return InetAddress.getByName(serverName)     //https://stackoverflow.com/questions/5806220/how-to-connect-to-my-http-localhost-web-server-from-android-emulator
     }
 
-    private fun getMySocket() : Socket {
+    private fun getMySocket() : Socket? {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
+        var sock : Socket? = null
 
-        return Socket(serverAddress, serverPort)
+        Thread {
+            try {
+                sock = Socket(serverAddress, serverPort)
+            }
+            finally {
+                println("Error Creating Socket")
+            }
+        }.start()
+
+        return sock
     }
 }
