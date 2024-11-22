884f74d9dc26fe8a54ab74110232878938931805
Joshua <joshbear367763@gmail.com>
2024-11-22 17:03:05 -0500

    Got the warnings out of the code: Removed some unused imports and constants Made use of the MessageResult enum class Added a switch for every command Renamed a variable in the sendToServer method to better reflect its role

--- app/src/main/java/com/example/myapplication/ServerConnect.kt
+++ app/src/main/java/com/example/myapplication/ServerConnect.kt
@@ -1,15 +1,11 @@
 package com.example.myapplication
 import android.os.StrictMode
-import androidx.collection.emptyLongSet
-import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
-import java.lang.Byte
 import java.net.InetAddress
 import java.net.Socket
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
-import java.util.*
 
 enum class Command(val int : Int) {
     SocketError(-1),
@@ -22,14 +18,20 @@ enum class Command(val int : Int) {
     GetUser(6),
     BanUser(7),
 }
-enum class MessageResult {
-    Failed,
-    LoginSuccess,
-    Success,
+
+enum class MessageResult(val int : Int) {
+
+    Failed(0),
+    LoginSuccess(1),
+    Success(2);
+
+    companion object {
+        fun fromInt(int : Int) = entries.first {it.int == int}
+    }
 }
 
 
-class ServerConnect() {
+class ServerConnect {
 
 
     private val serverAddress = getMyServerAddress()
@@ -41,7 +43,7 @@ class ServerConnect() {
 
     private fun getMyServerAddress() : InetAddress {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy);
+        StrictMode.setThreadPolicy(policy)
 
         return InetAddress.getByName(SERVER_NAME)     //https://stackoverflow.com/questions/5806220/how-to-connect-to-my-http-localhost-web-server-from-android-emulator
     }
@@ -67,10 +69,15 @@ class ServerConnect() {
             val command = readHeader()
             when (command)
             {
-                Command.SocketError.int -> error("Socket error")
-                Command.Failed.int -> error("Server disconnected")
+                Command.SocketError.int -> println("Socket error")
+                Command.Failed.int -> println("Server disconnected")
                 Command.Login.int -> handleToken()
-                else -> error("Received unexpected command")
+                Command.GetUsers.int -> {}
+                Command.MessageServer.int -> {}
+                Command.DiscussionPost.int -> {}
+                Command.GetUser.int -> {}
+                Command.BanUser.int -> {} //This should never be implemented
+                else -> println("Received unexpected command")
             }
         }
     }
@@ -81,8 +88,12 @@ class ServerConnect() {
         return ByteBuffer.wrap(buffer).order(ENDIAN).getInt()
     }
 
+    private fun messageResult() : MessageResult {
+        return MessageResult.fromInt(readHeader())
+    }
+
     private fun handleToken() {
-        inputStream?.skip(Int.SIZE_BYTES.toLong())
+        println(messageResult())
         val bytesToRead = readHeader()
 
         token = ByteArray(bytesToRead + 1 )
@@ -91,33 +102,29 @@ class ServerConnect() {
 
     }
 
-    //TODO: Remove this
-    //  NOTE:
-    //      When doing the login, fields are \n delimited
-
     fun sendToServer(command : Int, message : String)
     {
         val tokenSize = if (token != null)
             HASH_SIZE + 1 else 0
 
         val lengthOfMessage = message.length + LENGTH_OF_COMMAND_AND_MESSAGE_HEADER
-        val messageToServer = ByteArray(1024)
+
         if (outputStream == null)
             outputStream = socket?.getOutputStream()
 
-        var buffer : ByteArray = ByteArray(0)
+        var messageToServer = ByteArray(0)
         //Write command
-        buffer += ByteBuffer.allocate(Int.SIZE_BYTES).order(ENDIAN).putInt(command).array()
+        messageToServer += ByteBuffer.allocate(Int.SIZE_BYTES).order(ENDIAN).putInt(command).array()
 
         if (token != null) {
-            buffer += ByteBuffer.allocate(Int.SIZE_BYTES).order(ENDIAN).putInt(tokenSize).array()
-            buffer += token!!
+            messageToServer += ByteBuffer.allocate(Int.SIZE_BYTES).order(ENDIAN).putInt(tokenSize).array()
+            messageToServer += token!!
         }
 
-            buffer += ByteBuffer.allocate(Int.SIZE_BYTES).order(ENDIAN).putInt(lengthOfMessage).array()
-            buffer += message.toByteArray() + 0x00
+            messageToServer += ByteBuffer.allocate(Int.SIZE_BYTES).order(ENDIAN).putInt(lengthOfMessage).array()
+            messageToServer += message.toByteArray() + 0x00
 
-        outputStream?.write(buffer)
+        outputStream?.write(messageToServer)
         outputStream?.flush()
     }
 
@@ -139,9 +146,6 @@ class ServerConnect() {
         private const val SERVER_NAME = "10.0.2.2"
         private const val PORT = 5930
         private const val HASH_SIZE = 32
-        private const val NAME_SIZE = 50
-        private const val USERNAME_SIZE = 30
-        private const val PASSWORD_SIZE = 60
         private const val LENGTH_OF_COMMAND_AND_MESSAGE_HEADER = Int.SIZE_BYTES * 3 + 1
         //server endian
         private val ENDIAN = ByteOrder.LITTLE_ENDIAN
