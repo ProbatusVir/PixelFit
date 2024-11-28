60eb29a6dc089a662e0ba061f3128052158d68fa
Devin <DRMcLaughlin@student.fullsail.edu>
2024-11-28 02:19:36 -0500

    added functionalty to MainActivity.kt so that settings button will take us to settings fragment and not break when it is already on settings screen and settings is chosen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.widget.ImageButton
 import android.widget.PopupMenu
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -22,8 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-    private val connection = ServerConnect();
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,30 +26,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val profile = findViewById<ImageButton>(R.id.user_avatar)
-
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
-
-        val navView : BottomNavigationView = binding.bottomNav
-
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
+
+
+        val navView: BottomNavigationView = binding.bottomNav
         navView.setupWithNavController(navController)
 
-        val appBarConfiguration = AppBarConfiguration(
+
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.HomeFragment, R.id.FriendsFragment, R.id.GroupFragment, R.id.InstructorFragment
             )
-
         )
+
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.LoginFragment -> {
-                    bottomNav.visibility = View.GONE
-                    profile.visibility = View.GONE
-                }
-                R.id.signUp-> {
+                R.id.LoginFragment, R.id.signUp -> {
                     bottomNav.visibility = View.GONE
                     profile.visibility = View.GONE
                 }
@@ -65,47 +56,50 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        //click register for making the popup
-       findViewById<ImageButton>(R.id.user_avatar).setOnClickListener {
-           val popup = PopupMenu(this, it)
-           val inflater: MenuInflater = popup.menuInflater
-           inflater.inflate(R.menu.menu_main, popup.menu)
-           popup.show()
+        profile.setOnClickListener { view ->
+            showUserAvatarMenu(view, navController)
         }
     }
 
-    //implement this when the screens (fragments???) exist
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId)
-        {
-            R.id.edit_profile_menuitem ->
-            {
-                return true
-            }
-            R.id.goals_trophies_menuitem ->
-            {
-                return true
-            }
-            R.id.block_list_menuitem ->
-            {
-                return true
-            }
-            R.id.pending_duels_menuitem ->
-            {
-                return true
-            }
-            R.id.settings_menuitem ->
-            {
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
+    private fun showUserAvatarMenu(view: View, navController: androidx.navigation.NavController) {
 
+        val popup = PopupMenu(this, view)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.menu_main, popup.menu)
+
+        // Handle menu item clicks
+        popup.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.edit_profile_menuitem -> {
+                    true
+                }
+                R.id.goals_trophies_menuitem -> {
+                    true
+                }
+                R.id.block_list_menuitem -> {
+                    true
+                }
+                R.id.pending_duels_menuitem -> {
+                    true
+                }
+                R.id.settings_menuitem -> {
+                    if(navController.currentDestination?.id == R.id.SettingsFragment) {
+                        return@setOnMenuItemClickListener false
+                    }
+                    else {
+                        navController.navigate(R.id.action_HomeFragment_to_SettingsFragment)
+                    }
+                    true
+                }
+                else -> false
+            }
         }
+
+        popup.show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
