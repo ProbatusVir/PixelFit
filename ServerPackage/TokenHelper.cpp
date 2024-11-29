887d7ea0dd760a4b866fa627192cf63df8cad531
Joshua <joshbear367763@gmail.com>
2024-11-29 15:47:15 -0500

    Moved the Tokenizer methods to a .cpp file as it exceeded its original scope.

---
+++ ServerPackage/TokenHelper.cpp
@@ -0,0 +1,47 @@
+#include "TokenHelper.h"
+
+Tokenizer::Tokenizer(const char* message, const char delim)
+{
+	//Determine number of fields
+	const unsigned int message_length = strlen(message);
+	m_fields = 1;
+
+	for (int i = 0; i < message_length; i++)
+		m_fields += message[i] == delim;
+
+	Initialize(message, delim);
+}
+
+void Tokenizer::Initialize(const char* message, const char delim)
+{
+	const char delimiter[] = { delim, '\0' };
+	m_container = new char* [m_fields];
+	const char* seeker = message;
+	unsigned char token_length = 0;
+
+	for (unsigned int i = 0; i < m_fields; i++)
+	{
+		if (i < m_fields - 1)
+			token_length = strpbrk(seeker, delimiter) - seeker;
+		else
+			token_length = strlen(seeker);
+
+		m_container[i] = new char[token_length + 1];
+
+		memcpy_s(m_container[i], token_length + 1, seeker, token_length);
+		m_container[i][token_length] = '\0';
+
+		seeker += token_length + 1;
+	}
+}
+
+void Tokenizer::DestroyTokens()
+{
+	for (int i = 0; i < m_fields; i++)
+		delete[] m_container[i];
+
+	if (m_container)
+		delete[] m_container;
+}
+
+
--- ServerPackage/TokenHelper.h
+++ ServerPackage/TokenHelper.h
@@ -1,6 +1,5 @@
 
 #include <string.h>
-#include <algorithm>
 class Tokenizer
 {
 
@@ -13,33 +12,13 @@ public:
 	}
 
 	//Not sure how many fields there are? No problem.
-	Tokenizer(const char* message, const char delim = '\n')
-	{
-		//Determine number of fields
-		const unsigned int message_length = strlen(message);
-		m_fields = 1;
+	Tokenizer(const char* message, const char delim = '\n');
 
-		for (int i = 0; i < message_length; i++)
-			m_fields += message[i] == delim;
+	char** Tokens() { return m_container; }
 
-		Initialize(message, delim);
-	}
-
-	char** Tokens()
-	{
-		return m_container;
-	}
-
-	char* operator[](unsigned int n)
-	{
-		if (n >= 0 && n < m_fields)
-			return m_container[n];
-		
-		return nullptr;
-	}
+	char* operator[](unsigned int n) { return (n >= 0 && n < m_fields) ? m_container[n] : nullptr;}
 
 	operator char** () { return m_container; }
-
 
 	unsigned int Fields() {return m_fields;}
 
@@ -50,35 +29,7 @@ private:
 	char** m_container = nullptr;
 	unsigned int m_fields = 0;
 
-	void Initialize(const char* message, const char delim = '\n')
-	{
-		const char delimiter[] = { delim, '\0' };
-		m_container = new char* [m_fields];
-		const char* seeker = message;
-		unsigned char token_length = 0;
+	void Initialize(const char* message, const char delim = '\n');
 
-		for (unsigned int i = 0; i < m_fields; i++)
-		{
-			if (i < m_fields - 1)
-				token_length = strpbrk(seeker, delimiter) - seeker;
-			else
-				token_length = strlen(seeker);
-
-			m_container[i] = new char[token_length + 1];
-
-			memcpy_s(m_container[i], token_length + 1, seeker, token_length);
-			m_container[i][token_length] = '\0';
-
-			seeker += token_length + 1;
-		}
-	}
-
-	void DestroyTokens()
-	{
-		for (int i = 0; i < m_fields; i++)
-			delete[] m_container[i];
-
-		if (m_container)
-			delete[] m_container;
-	}
+	void DestroyTokens();
 };
