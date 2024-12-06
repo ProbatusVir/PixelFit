bc3c7b32d3eb2e5d6017954739f5192123833950
Joshua <joshbear367763@gmail.com>
2024-12-06 16:53:34 -0500

    Added a safer way to test over the internet, might have to get rid of it, but it's been pretty helpful for me

--- app/src/main/java/ServerConnect.kt
+++ app/src/main/java/ServerConnect.kt
@@ -264,7 +264,8 @@ class ServerConnect private constructor() {
         fun destroyInstance() {INSTANCE?.disconnect(); INSTANCE = null}
         private var INSTANCE : ServerConnect? = ServerConnect()
         private const val SERVER_NAME = "2.tcp.ngrok.io"
-        private const val LOCALHOST = "10.0.2.2"
+        private const val LOCALHOST = "2.tcp.ngrok.io"
+        //private const val LOCALHOST = "10.0.2.2"
         private const val PORT = 10214
         private const val HASH_SIZE = 32
         private const val LENGTH_OF_COMMAND_AND_MESSAGE_HEADER = Int.SIZE_BYTES * 3 + 1 //This is good for an authenticated read, might have to cut it out later.
