3927823a2deeb386bb39d3a9e1b16c6c0e34c240
Joshua <joshbear367763@gmail.com>
2024-12-08 01:11:20 -0500

    Kotlin is now the source of truth on ResourceType enums (the high values of the magic numbers causes parity issues)

--- app/src/main/java/ServerConnect.kt
+++ app/src/main/java/ServerConnect.kt
@@ -29,7 +29,7 @@ enum class Command(val int : Int) {
 
 enum class ResourceType(val int:  Int)
 {
-    PNG(-1991225785), //this is first four bytes of the PNG header.
+    PNG(0x89504E47.toInt()), //this is first four bytes of the PNG header.
 }
 
 enum class MessageResult(val int : Int) {
@@ -212,7 +212,8 @@ class ServerConnect private constructor() {
         //        + fileName.toByteArray())
 
         val fileNameAsBA = fileName.toByteArray() //.order(ENDIAN)
-        val properType = ByteBuffer.allocate(Int.SIZE_BYTES).putInt(type.int).array()
+        //val properType = ByteBuffer.allocate(Int.SIZE_BYTES).putInt(type.int).order(ENDIAN).array()
+        val properType = ByteBuffer.allocate(Int.SIZE_BYTES).putInt(type.int).order(ByteOrder.BIG_ENDIAN).array()
         val buffer = properType + fileNameAsBA
         sendToServer(Command.RequestData.int, buffer)
 
@@ -263,9 +264,9 @@ class ServerConnect private constructor() {
         fun instance() = INSTANCE
         fun destroyInstance() {INSTANCE?.disconnect(); INSTANCE = null}
         private var INSTANCE : ServerConnect? = ServerConnect()
-        private const val SERVER_NAME = "2.tcp.ngrok.io"
+        private const val SERVER_NAME = "8.tcp.ngrok.io"
         private const val LOCALHOST = "10.0.2.2"
-        private const val PORT = 10214
+        private const val PORT = 14982
         private const val HASH_SIZE = 32
         private const val LENGTH_OF_COMMAND_AND_MESSAGE_HEADER = Int.SIZE_BYTES * 3 + 1 //This is good for an authenticated read, might have to cut it out later.
         //server endian
