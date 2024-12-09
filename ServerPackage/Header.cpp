60ccd1b0c337ac8b409b08ba3ab61554dc62084a
Joshua <joshbear367763@gmail.com>
2024-12-09 13:05:28 -0500

    Added a new initialization for headers (might make it a new OutboundHeader class) Corrected some warnings

--- ServerPackage/Header.cpp
+++ ServerPackage/Header.cpp
@@ -22,12 +22,44 @@ Header::Header(const SOCKET socket, unsigned int wait)
 	recv(socket, buffer, buffer_size, 0);
 }
 
+Header::Header(const Command _cmd, const unsigned int _tk_size, const char* _token, const unsigned int _bf_size, const char* _buffer) :
+	token_size(_tk_size),
+	buffer_size(_bf_size)
+{
+	command = new Command(_cmd);
+	token = new char[token_size + 1]; token[token_size] = '\0';
+	buffer = new char[buffer_size + 1]; buffer[buffer_size] = '\0';
+	memcpy_s(token,  token_size,  _token,  token_size);
+	memcpy_s(buffer, buffer_size, _buffer, buffer_size);
+
+}
+
 
 
 Header::~Header()
 {
+	if (command)
+		delete command;
 	if (token)
 		delete[] token;
 	if (buffer)
 		delete[] buffer;
+	if (serialized)
+		delete[] serialized;
+}
+
+const char* Header::Serialize()
+{
+	if (serialized) //This might not be best practice, but I'd rather avoid bikeshedding.
+		delete[] serialized;
+	
+	serialized_size = sizeof(command) + sizeof(token_size) + token_size + sizeof(buffer_size) + buffer_size;
+	serialized = new char[serialized_size];
+	char* write_point;
+	memcpy_s(write_point = serialized,				sizeof(*command),		command,		sizeof(*command));
+	memcpy_s(write_point += sizeof(*command),		sizeof(token_size),		&token_size,	sizeof(token_size));
+	memcpy_s(write_point += sizeof(token_size),		token_size,				token,			token_size);
+	memcpy_s(write_point += token_size,				sizeof(buffer_size),	&buffer_size,	sizeof(buffer_size));
+	memcpy_s(write_point += sizeof(buffer_size),	buffer_size,			&buffer,		buffer_size);
+	return serialized;
 }
--- ServerPackage/Header.h
+++ ServerPackage/Header.h
@@ -1,14 +1,22 @@
 #pragma once
 #include <winsock.h>
+#include "Constants.h"
+
 struct Header
 {
 	Header(const SOCKET socket, unsigned int wait = 0);
+	Header(const Command _cmd, const unsigned int _tk_size, const char* _token, const unsigned int _bf_size, const char* _buffer);
 	~Header();
+	//This is managed by the struct. Do not delete the returned pointer.
+	const char* Serialize();
 	static unsigned int ReadByteHeader(const SOCKET clientSocket);
+
+	Command* command = nullptr; //I don't love this, but command should be nullable.
 	unsigned int token_size;
-	char* token;
+	char* token = nullptr;
 	unsigned int buffer_size;
-	char* buffer;
-	
+	char* buffer = nullptr;
+	char* serialized = nullptr;
+	int serialized_size = -1;
 };
 
