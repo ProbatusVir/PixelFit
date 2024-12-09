dfb36fd1627edc125a7ec16b75440c156f6a0fd5
Joshua <joshbear367763@gmail.com>
2024-12-09 17:52:23 -0500

    Further compartmentalization of the Header with possibility of expansion

--- ServerPackage/Header.cpp
+++ ServerPackage/Header.cpp
@@ -1,6 +1,11 @@
 #include "Header.h"
 #include <thread>
 #include <chrono>
+
+const char* HackyConcatenator(const size_t fields, const char** data, const size_t* sizes);
+const char* HackyConcatenator(const size_t fields, const char** data, const size_t* sizes, char*& array, size_t& array_size);
+
+
 unsigned int Header::ReadByteHeader(const SOCKET clientSocket)
 {
 	unsigned int byteHeader = 0;
@@ -8,13 +13,36 @@ unsigned int Header::ReadByteHeader(const SOCKET clientSocket)
 	return byteHeader;
 }
 
-Header::Header(const SOCKET socket, unsigned int wait)
+Header::Header(const SOCKET socket)
 {
 	token_size = ReadByteHeader(socket);
 	token = new char[token_size + 1];
 	recv(socket, (char*)token, token_size, 0);
-	
+
 	buffer_size = ReadByteHeader(socket);
+}
+
+Header::Header(const unsigned int _tk_size, const char* _token, const unsigned int _bf_size) :
+	token_size(_tk_size),
+	buffer_size(_bf_size)
+{
+	token = new char[token_size + 1]; token[token_size] = '\0';
+	memcpy_s(token, token_size, _token, token_size);
+}
+
+Header::~Header()
+{
+	if (token)
+		delete[] token;
+	if (serialized)
+		delete[] serialized;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Packet::Packet(const SOCKET socket, unsigned int wait) :
+	Header(socket)
+{
 	buffer = new char[buffer_size + 1];
 
 	//This is awful... please don't keep this...
@@ -22,44 +50,133 @@ Header::Header(const SOCKET socket, unsigned int wait)
 	recv(socket, buffer, buffer_size, 0);
 }
 
-Header::Header(const Command _cmd, const unsigned int _tk_size, const char* _token, const unsigned int _bf_size, const char* _buffer) :
-	token_size(_tk_size),
-	buffer_size(_bf_size)
+Packet::Packet(const unsigned int _tk_size, const char* _token, const unsigned int _bf_size, const char* _buffer) :
+	Header(_tk_size, _token, _bf_size)
 {
-	command = new Command(_cmd);
-	token = new char[token_size + 1]; token[token_size] = '\0';
 	buffer = new char[buffer_size + 1]; buffer[buffer_size] = '\0';
-	memcpy_s(token,  token_size,  _token,  token_size);
 	memcpy_s(buffer, buffer_size, _buffer, buffer_size);
-
 }
 
 
 
-Header::~Header()
+Packet::~Packet()
 {
-	if (command)
-		delete command;
-	if (token)
-		delete[] token;
-	if (buffer)
+	if(buffer)
 		delete[] buffer;
-	if (serialized)
-		delete[] serialized;
 }
 
-const char* Header::Serialize()
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+OutboundHeader::OutboundHeader(Command _command, const unsigned int _tk_size, const char* _token, const unsigned int _bf_size) :
+	InboundHeader(_tk_size, _token, _bf_size)
 {
-	if (serialized) //This might not be best practice, but I'd rather avoid bikeshedding.
-		delete[] serialized;
+	command = _command;
+}
+
+const char* OutboundHeader::Serialize()
+{
+	//commmand ts, t, bs, buffer			
+	const char* data[] = { (const char*)&command, (const char*)&token_size, (const char*)token, (const char*)&buffer_size};
+	const size_t sizes[] = { sizeof(command),		sizeof(token_size),		token_size,			sizeof(buffer_size)};
+	constexpr static const size_t fields = sizeof(sizes) / sizeof(sizes[0]);
+
+	return HackyConcatenator(fields, data, sizes, serialized, serialized_size);
+	static_assert(sizeof(sizes) / sizeof(sizes[0]) == sizeof(data) / sizeof(data[0]));
+}
+
+OutboundPacket::OutboundPacket(Command _command, const unsigned int _tk_size, const char* _token, const unsigned int _bf_size, const char* _buffer) :
+	InboundPacket(_tk_size, _token, _bf_size, _buffer)
+{
+	command = _command;
+}
+
+const char* OutboundPacket::Serialize()
+{
+	//commmand ts, t, bs, buffer			
+	const char* data[] = { (const char*)&command, (const char*)&token_size, (const char*)token, (const char*)&buffer_size,	buffer };
+	const size_t sizes[] = { sizeof(command),		sizeof(token_size),		token_size,			sizeof(buffer_size),		buffer_size };
+	constexpr static const size_t fields = sizeof(sizes) / sizeof(sizes[0]);
+
+	return HackyConcatenator(fields, data, sizes, serialized, serialized_size);
+	static_assert(sizeof(sizes) / sizeof(sizes[0]) == sizeof(data) / sizeof(data[0]));
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const char* InboundHeader::Serialize()
+{
+	//ts, t, bs
+	const char* data[] = { (const char*)&token_size, (const char*)token, (const char*)&buffer_size };
+	const size_t sizes[] = { sizeof(token_size),		token_size,			sizeof(buffer_size) };
+	constexpr static const size_t fields = sizeof(sizes) / sizeof(sizes[0]);
 	
-	serialized_size = sizeof(command) + sizeof(token_size) + token_size + sizeof(buffer_size) + buffer_size;
-	serialized = new char[serialized_size];
+	return HackyConcatenator(fields , data, sizes, serialized, serialized_size);
+	static_assert(sizeof(sizes) / sizeof(sizes[0]) == sizeof(data) / sizeof(data[0]));
+}
+
+const char* InboundPacket::Serialize()
+{
+	//ts, t, bs, buffer			
+	const char* data[] = { (const char*)&token_size, (const char*)token, (const char*)&buffer_size, buffer };
+	const size_t sizes[] = { sizeof(token_size),		token_size,			sizeof(buffer_size),	buffer_size };
+	constexpr static const size_t fields = sizeof(sizes) / sizeof(sizes[0]);
+
+	return HackyConcatenator(fields, data, sizes, serialized, serialized_size);
+	static_assert(sizeof(sizes) / sizeof(sizes[0]) == sizeof(data) / sizeof(data[0]));
+}
+
+//I've been wanting to do something like this, but I still need to workshop this idea...
+const char* HackyConcatenator(const size_t fields, const char** data, const size_t* sizes)
+{
+	size_t array_size = 0;
+	char* array;
 	char* write_point;
-	memcpy_s(write_point = serialized,				sizeof(*command),		command,		sizeof(*command));
-	memcpy_s(write_point += sizeof(*command),		sizeof(token_size),		&token_size,	sizeof(token_size));
-	memcpy_s(write_point += sizeof(token_size),		token_size,				token,			token_size);
-	memcpy_s(write_point += token_size,				sizeof(buffer_size),	&buffer_size,	sizeof(buffer_size));
-	memcpy_s(write_point += sizeof(buffer_size),	buffer_size,			&buffer,		buffer_size);
-	return serialized;
+	//Get buffer size
+	for (int i = 0; i < fields; i++)
+		array_size += sizes[i];
+
+	array = new char[array_size];
+	write_point = array;
+	
+	//Fill buffer
+	for (int i = 0; i < fields; i++)
+	{
+		memcpy(write_point, data[i], sizes[i]);
+		write_point += sizes[i];
+	}
+
+	return array;
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="fields">The number of items in the data and sizes arrays</param>
+/// <param name="data">An array of data</param>
+/// <param name="sizes:">Array of sizes corresponding to the data values' sizes</param>
+/// <param name="arr:">The pointer variable to be reassigned to point to the new data</param>
+/// <param name="arr_size:">Will be reassigned to the new array's size</param>
+/// <returns></returns>
+const char* HackyConcatenator(const size_t fields, const char** data, const size_t* sizes, char*& array, size_t& array_size)
+{
+	if (array)
+		delete[] array;
+	array_size = 0;
+
+	char* write_point;
+	//Get buffer size
+	for (int i = 0; i < fields; i++)
+		array_size += sizes[i];
+
+	array = new char[array_size];
+	write_point = array;
+
+	//Fill buffer
+	for (int i = 0; i < fields; i++)
+	{
+		memcpy(write_point, data[i], sizes[i]);
+		write_point += sizes[i];
+	}
+
+	return array;
 }
