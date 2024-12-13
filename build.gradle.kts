c99d7715d6db01ab02fb7c5bf7a8acf81f6a1ced
Rashid Byrdsell <RHByrdsell@outlook.com>
2024-12-12 19:29:04 -0500

    UnInstalling ExoPlayer(Couldn't get it to work)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply true
-    alias(libs.plugins.kotlin.android) apply true
-}
-dependencies {
-    implementation("com.google.android.exoplayer:exoplayer:2.19.1") // Core ExoPlayer library
-    implementation("com.google.android.exoplayer:exoplayer-ui:2.18.6") // UI module for ExoPlayer
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
 
