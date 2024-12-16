76a95f11ad6a343e9a250afbe1799cbe3e37a5b0
Rashid Byrdsell <RHByrdsell@outlook.com>
2024-12-16 14:22:15 -0500

    Included Video playback and pause when scrolling

--- app/src/main/java/InstructorAdapter.kt
+++ app/src/main/java/InstructorAdapter.kt
@@ -9,6 +9,9 @@ import com.example.myapplication.R
 
 class InstructorAdapter(var mList : List<InstructorData>) : RecyclerView.Adapter<InstructorAdapter.InstructorViewHolder>() {
 
+    private var playerPos : Int? = null
+    private var playerView : VideoView? = null
+
     inner class InstructorViewHolder(v : View) : RecyclerView.ViewHolder(v) {
         val card : CardView = v.findViewById(R.id.firstCard)
         val video : VideoView = v.findViewById(R.id.video1)
@@ -35,6 +38,12 @@ class InstructorAdapter(var mList : List<InstructorData>) : RecyclerView.Adapter
 
     override fun onBindViewHolder(holder : InstructorViewHolder, position: Int) {
         holder.video.setVideoPath(mList[position].videoURL)
+        //prepares the video
+        holder.video.setOnCompletionListener {
+            playerPos = null
+            playerView = null
+
+        }
         holder.titleTv.text = mList[position].title
         holder.description.text = mList[position].description
 //        holder.equipment.text = mList[position].equipment
@@ -44,11 +53,33 @@ class InstructorAdapter(var mList : List<InstructorData>) : RecyclerView.Adapter
             it.pause()
         }
         holder.card.setOnClickListener {
+//            if(playerPos != mList[position].videoURL.toInt()) {
+//                playerView?.stopPlayback()
+//                playerView?.visibility = View.GONE
+//
+//                playerPos = mList[position].videoURL.toInt()
+//                playerView = holder.video
+//
+//                //Start playback
+//                holder.video.visibility = View.VISIBLE
+//                holder.video.start()
+//            }
             if(!holder.video.isPlaying){
                 holder.video.start()
             }else{
                 holder.video.pause()
             }
         }
+    }
+
+    override fun onViewRecycled(holder : InstructorViewHolder) {
+        super.onViewRecycled(holder)
+
+//        if(holder.video == playerView) {
+//            playerView?.stopPlayback()
+//            playerView?.visibility = View.GONE
+//            playerView = null
+//            playerPos = null
+//        }
     }
 }
