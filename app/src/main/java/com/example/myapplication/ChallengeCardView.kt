0f42659ec186e03ee35e4767ea3c657cf4c131ea
Joshua <joshbear367763@gmail.com>
2024-12-21 21:26:01 -0500

    ActiveUser is updated by accepting. Updated are the time spent exercising, the parts worked out, and the calories burned.

--- app/src/main/java/com/example/myapplication/ChallengeCardView.kt
+++ app/src/main/java/com/example/myapplication/ChallengeCardView.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentChallengeCardViewBinding
-
+import ActiveUser
 
 
 
@@ -30,6 +30,18 @@ class ChallengeCardView : Fragment() {
 
         binding.challengeAcceptButton.setOnClickListener {
             findNavController().navigate(R.id.GroupFragment)
+            if (Shared.currentChallenge != null) {
+                ActiveUser.caloriesBurned += Shared.currentChallenge!!.calories
+                ActiveUser.timeSpentExercising += Shared.currentChallenge!!.minutes
+
+
+                for (index in ActiveUser.partsWorked.indices) {
+                    //wow, this is awful!
+                    ActiveUser.partsWorked[index] = ActiveUser.partsWorked[index] || Shared.currentChallenge!!.partsWorked[index]
+                }
+            }
+
+
         }
 
         binding.challengeDeclineButton.setOnClickListener {
@@ -41,7 +53,6 @@ class ChallengeCardView : Fragment() {
         binding.challengeDescription.text = challenge?.description
         binding.conditionText.text = challenge?.condition
         binding.punishmentText.text = challenge?.punishment
-        //binding.
 
     }
 
